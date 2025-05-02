6520d1666c2c0f9de0a7d2971a2cf61a4b1c015b
chxghee <francis001021@gmail.com>
2025-05-02 11:41:41 +0900

    Refactor: 리뷰 반영 ReservationApiController URI mapping  @RequsetMapping 으로 변경 컨트롤러 패키지 세분화 view, api

--- src/main/java/roomescape/controller/api/ReservationApiController.java
+++ src/main/java/roomescape/controller/api/ReservationApiController.java
@@ -1,7 +1,8 @@
-package roomescape.controller;
+package roomescape.controller.api;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.dto.ReservationResponse;
 import roomescape.entity.Reservation;
@@ -9,11 +10,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-public class ReservationController {
+@RequestMapping("/reservations")
+public class ReservationApiController {
 
     private List<Reservation> reservations = new ArrayList<>();
 
-    @GetMapping("/reservations")
+    @GetMapping
     public ResponseEntity<List<ReservationResponse>> getReservations() {
         List<ReservationResponse> reservationResponseList = reservations.stream()
                 .map(ReservationResponse::new)
