cb55e5886cefa850b861e77280711e60d179f8ba
chxghee <francis001021@gmail.com>
2025-05-04 01:01:11 +0900

    Feat: 3단계 구현
    예약추가 삭제 API 구현
    Service Layer 추가

--- src/main/java/roomescape/dto/ReservationRequest.java
+++ src/main/java/roomescape/dto/ReservationRequest.java
@@ -1,4 +1,36 @@
 package roomescape.dto;
 
+
+import roomescape.entity.Reservation;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class ReservationRequest {
+
+    private final LocalDate date;
+    private final String name;
+    private final LocalTime time;
+
+    public ReservationRequest(LocalDate date, String name, LocalTime time) {
+        this.date = date;
+        this.name = name;
+        this.time = time;
+    }
+
+    public Reservation toEntity(Long id) {
+        return new Reservation(id, name, date, time);
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
 }
--- src/main/java/roomescape/service/ReservationService.java
+++ src/main/java/roomescape/service/ReservationService.java
@@ -1,4 +1,34 @@
 package roomescape.service;
 
+import org.springframework.stereotype.Service;
+import roomescape.dto.ReservationRequest;
+import roomescape.dto.ReservationResponse;
+import roomescape.entity.Reservation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
 public class ReservationService {
+
+    private AtomicLong index = new AtomicLong(0);
+    private List<Reservation> reservations = new ArrayList<>();
+
+    public List<ReservationResponse> getAllReservations() {
+        return reservations.stream()
+                .map(ReservationResponse::new)
+                .toList();
+    }
+
+    public ReservationResponse createReservation(ReservationRequest reservationRequest) {
+        Reservation newReservation = reservationRequest.toEntity(index.incrementAndGet());
+        reservations.add(newReservation);
+        return new ReservationResponse(newReservation);
+    }
+
+    public void deleteReservation(Long reservationId) {
+        reservations.removeIf(reservation -> reservation.getId().equals(reservationId));
+    }
+
 }
