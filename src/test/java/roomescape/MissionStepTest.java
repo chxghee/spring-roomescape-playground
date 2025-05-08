23b8446b4926a3a1a3c65202796e91e3dfda4cbd
chxghee <francis001021@gmail.com>
2025-05-08 23:48:13 +0900

    Refactor: API테스트 코드 @Nested로 변경 및 예외상황시 테스트 추가

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -2,10 +2,11 @@ package roomescape;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
-
+import org.junit.jupiter.api.Nested;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,73 +24,122 @@ public class MissionStepTest {
                 .statusCode(200);
     }
 
-    @Test
-    void 이단계() {
-        RestAssured.given().log().all()
-                .when().get("/reservation")
-                .then().log().all()
-                .statusCode(200);
+    @Nested
+    class 이단계 {
+        @Test
+        public void 예약확인페이지로_이동하면_200_응답을_전송해야한다() {
+            RestAssured.given().log().all()
+                    .when().get("/reservation")
+                    .then().log().all()
+                    .statusCode(200);
+        }
 
-        RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(0));
+        @Test
+        public void 예약_조회_요청이_성공하면_200_응답을_전송해야한다() {
+            RestAssured.given().log().all()
+                    .when().get("/reservations")
+                    .then().log().all()
+                    .statusCode(200)
+                    .body("size()", is(0));
+        }
     }
 
-    @Test
-    void 삼단계() {
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "브라운");
-        params.put("date", "2023-08-05");
-        params.put("time", "15:40");
+    @Nested
+    class 삼단계 {
 
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(params)
-                .when().post("/reservations")
-                .then().log().all()
-                .statusCode(201)
-                .header("Location", "/reservations")
-                .body("id", is(1));
+        private Map<String, String> params;
 
-        RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(1));
+        @BeforeEach
+        void setUp() {
+            params = new HashMap<>();
+            params.put("name", "브라운");
+            params.put("date", "2023-08-05");
+            params.put("time", "15:40");
+        }
 
-        RestAssured.given().log().all()
-                .when().delete("/reservations/1")
-                .then().log().all()
-                .statusCode(204);
+        @Test
+        public void 예약_등록_요청이_성공하면_201_응답을_전송해야한다() {
+            RestAssured.given().log().all()
+                    .contentType(ContentType.JSON)
+                    .body(params)
+                    .when().post("/reservations")
+                    .then().log().all()
+                    .statusCode(201)
+                    .header("Location", "/reservations")
+                    .body("id", is(1));
+        }
 
-        RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(0));
+        @Test
+        public void 예약_조회_요청이_성공하면_200_응답을_전송해야한다() {
+            RestAssured.given().log().all()
+                    .when().get("/reservations")
+                    .then().log().all()
+                    .statusCode(200)
+                    .body("size()", is(0));
+        }
+
+        @Test
+        public void 예약_삭제_요청이_성공하면_204_응답을_전송해야한다() {
+
+            RestAssured.given().log().all()
+                    .contentType(ContentType.JSON)
+                    .body(params)
+                    .when().post("/reservations")
+                    .then().log().all()
+                    .statusCode(201)
+                    .header("Location", "/reservations")
+                    .body("id", is(1));
+
+            RestAssured.given().log().all()
+                    .when().delete("/reservations/1")
+                    .then().log().all()
+                    .statusCode(204);
+        }
     }
 
-    @Test
-    void 사단계() {
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "브라운");
-        params.put("date", "");
-        params.put("time", "");
+    @Nested
+    class 사단계 {
+        private Map<String, String> params;
 
-        // 필요한 인자가 없는 경우
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(params)
-                .when().post("/reservations")
-                .then().log().all()
-                .statusCode(400);
+        @Test
+        public void 삭제할_예약이_없다면_성공하면_400_응답을_전송해야한다() {
+            RestAssured.given().log().all()
+                    .when().delete("/reservations/1")
+                    .then().log().all()
+                    .statusCode(400)
+                    .body("title", is("예약 정보 없음"));
+        }
 
-        // 삭제할 예약이 없는 경우
-        RestAssured.given().log().all()
-                .when().delete("/reservations/1")
-                .then().log().all()
-                .statusCode(400);
+        @Test
+        public void 빈_값으로_예약_등록_요청하면_400_응답을_전송해야한다() {
+            Map<String, String> emptyParams = new HashMap<>();
+            emptyParams.put("name", "");
+            emptyParams.put("date", "2023-08-05");
+            emptyParams.put("time", "15:40");
+
+            RestAssured.given().log().all()
+                    .contentType(ContentType.JSON)
+                    .body(emptyParams)
+                    .when().post("/reservations")
+                    .then().log().all()
+                    .statusCode(400)
+                    .body("title", is("필수 입력값 누락"));
+        }
+
+        @Test
+        public void 올바르지_않는_시간_형식으로_예약_등록_요청하면_400_응답을_전송해야한다() {
+            Map<String, String> emptyParams = new HashMap<>();
+            emptyParams.put("name", "브라운");
+            emptyParams.put("date", "2023-8-5");
+            emptyParams.put("time", "15:40");
+
+            RestAssured.given().log().all()
+                    .contentType(ContentType.JSON)
+                    .body(emptyParams)
+                    .when().post("/reservations")
+                    .then().log().all()
+                    .statusCode(400)
+                    .body("title", is("시간 포맷 오류"));
+        }
     }
 }
