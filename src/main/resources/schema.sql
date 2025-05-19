09b4541375f976bd95486aa7afedb1155b8f359c
chxghee <francis001021@gmail.com>
2025-05-20 02:36:12 +0900

    fix: schema.sql 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,16 +1,17 @@
-CREATE TABLE IF NOT EXISTS reservation
-(
-    id      BIGINT       NOT NULL AUTO_INCREMENT,
-    name    VARCHAR(255) NOT NULL,
-    date    DATE NOT NULL,
-    time    TIME NOT NULL,
-    PRIMARY KEY (id)
-);
-
-
 CREATE TABLE IF NOT EXISTS time
 (
     id   BIGINT       NOT NULL AUTO_INCREMENT,
     time TIME NOT NULL,
     PRIMARY KEY (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS reservation
+(
+    id   BIGINT       NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    time_id BIGINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (time_id) REFERENCES time(id)
 );
