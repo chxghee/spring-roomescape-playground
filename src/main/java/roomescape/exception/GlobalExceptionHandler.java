d258fb30492b79f90bfd4d337af2d3103113221a
chxghee <francis001021@gmail.com>
2025-05-20 13:02:46 +0900

    refactor: GlobalExceptionHandler 수정 @ResponseStatus 방식에서 ResponseEntity로 응답

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package roomescape.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,30 +11,30 @@ import java.time.format.DateTimeParseException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    
     @ExceptionHandler(NotFoundException.class)
-    public ErrorResult notFoundException(NotFoundException e, HttpServletRequest request) {
-        return new ErrorResult(e.getTitle(), 400, e.getMessage(), request.getRequestURI());
+    public ResponseEntity<ErrorResult> notFoundException(NotFoundException e, HttpServletRequest request) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResult(e.getTitle(), 400, e.getMessage(), request.getRequestURI()));
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(DateTimeParseException.class)
-    public ErrorResult dateTimeFormatExceptionHandler(DateTimeParseException e, HttpServletRequest request) {
-        return new ErrorResult("시간 포맷 오류", 400, "시간은 yyyy-MM-dd / HH:mm 형식이어야 합니다.", request.getRequestURI());
+    public ResponseEntity<ErrorResult> dateTimeFormatExceptionHandler(DateTimeParseException e, HttpServletRequest request) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResult("시간 포맷 오류", 400, "시간은 yyyy-MM-dd / HH:mm 형식이어야 합니다.", request.getRequestURI()));
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(EmptyValueException.class)
-    public ErrorResult emptyValueException(EmptyValueException e, HttpServletRequest request) {
-        return new ErrorResult(e.getTitle(), 400, e.getMessage(), request.getRequestURI());
+    public ResponseEntity<ErrorResult> emptyValueException(EmptyValueException e, HttpServletRequest request) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResult(e.getTitle(), 400, e.getMessage(), request.getRequestURI()));
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(RuntimeException.class)
-    public ErrorResult runtimeExceptionHandler(RuntimeException e, HttpServletRequest request) {
+    public ResponseEntity<ErrorResult> runtimeExceptionHandler(RuntimeException e, HttpServletRequest request) {
         System.out.println("잡힌 예외 타입: " + e.getClass().getName());  // 로그
         System.out.println("메세지: " + e.getMessage());
-        return new ErrorResult("알 수 없는 오류", 400, "관리자에게 문의 하세요", request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResult("알 수 없는 오류", 500, "관리자에게 문의 하세요", request.getRequestURI()));
     }
 }
